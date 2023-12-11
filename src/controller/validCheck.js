65d77d2d924a2123b3ed483a2806c7084b65dc00
binz1215 <binz1215@naver.com>
2023-12-12 02:26:23 +0900

    valid: 유효성 검사 객체 작성

---
+++ src/controller/validCheck.js
@@ -0,0 +1,54 @@
+import { inputView } from '../view/inputView.js';
+
+class Valid {
+  // #car;
+
+  // #times;
+
+  // constructor() {
+  //  this.#car = inputView.carName();
+  //  this.#times = inputView.attemptTime();
+  // }
+
+  async validateCar() {
+    const car = await inputView.carName();
+    const eachCar = car.split(',');
+    this.#validateLength(eachCar);
+    this.#validateDuple(eachCar);
+  }
+
+  async validateTime() {
+    const time = await inputView.attemptTime();
+    this.#validateNumber(time);
+  }
+
+  /**
+   *
+   * @param {Array} car
+   */
+  #validateLength(car) {
+    if (!car.every((name) => name.length <= 5 && name.length > 0)) {
+      throw new Error(`[ERROR] 이름은 5자 이하만 가능합니다.`);
+    }
+  }
+
+  #validateDuple(car) {
+    const carSet = new Set(car);
+    if (car.length !== carSet.size) {
+      throw new Error(`[ERROR] 중복되는 이름은 사용할 수 없습니다.`);
+    }
+  }
+
+  #validateNumber(time) {
+    const timeNum = Number(time);
+    if (Number.isNaN(timeNum) || timeNum < 1) {
+      throw new Error(`[ERROR] 유효한 숫자형태가 아닙니다.`);
+    }
+  }
+}
+
+export default Valid;
+
+// const valid = new Valid();
+/// / valid.validateCar();
+// valid.validateTime();
